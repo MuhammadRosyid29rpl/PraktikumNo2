22ab41f8e9ebd0baab3579efe3160d0e7c4037ec
MuhammadRosyid29rpl <muhammad_rosyid_29rpl@student.smktelkom-mlg.sch.id>
2021-01-14 22:00:44 +0700

    PraktikumNo2

---
+++ src/praktikumno2/PraktikumNo2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikumno2;
+
+/**
+ *
+ * @author ASUS TUF A15
+ */
+public class PraktikumNo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    for (int i = 1; i <=4; i++) {  //metodeperulangan
+            for (int j = 1; j <=i; j++) {   //i dan j saling berhubungan
+                System.out.print("@");      
+            }System.out.println("");
+    }
+    
+}
+}
